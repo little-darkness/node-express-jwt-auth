19b010e0ae15d30b4202f88f4cb71eff14119618
Illia Starodubtcev <staroi01@kits.kfplc.com>
2023-01-03 19:23:52 +0200

    add jwt token generation for login and sigup post

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,44 +1,81 @@
+const jwt = require('jsonwebtoken')
 const User = require("../models/User")
 
 // handle error
 const handleErrors = (errors) => {
-  console.log(errors.message, errors.code)
-  let customErrors = {email: '', password: ''}
+    let customErrors = {email: '', password: ''}
 
-  // duplicate error code
-  if (errors.code === 11000) {
-    customErrors.email = 'Email already exists'
+    // duplicate error code
+    if (errors.code === 11000) {
+        customErrors.email = 'Email already exists'
+    }
+
+    // incorrect email
+    if (errors.message === 'No such user found') {
+        customErrors.email = 'No such user found'
+    }
+
+    // incorrect password
+    if (errors.message === 'Incorrect password') {
+        customErrors.password = 'Incorrect password'
+    }
+
+    // validation error
+    if (errors.message.includes('user validation failed')) {
+        Object.values(errors.errors).forEach(({properties}) => customErrors[properties.path] = properties.message)
+    }
     return Object.fromEntries(Object.entries(customErrors).filter(([key, value]) => value !== ''))
-  }
+}
 
-  // validation error
-  if (errors.message.includes('user validation failed')) {
-    Object.values(errors.errors).forEach(({properties}) => customErrors[properties.path] = properties.message)
-  }
-  return Object.fromEntries(Object.entries(customErrors).filter(([key, value]) => value !== ''))
+const maxAge = 3 * 24 * 60 * 60
+
+const createJwt = (userId) => {
+    return jwt.sign({userId}, process.env.USER_SECRET, {
+        expiresIn: maxAge
+    })
 }
 
 // controller actions
 module.exports.signup_get = (req, res) => {
-  res.render('signup')
+    res.render('signup')
 }
 
 module.exports.login_get = (req, res) => {
-  res.render('login')
+    res.render('login')
 }
 
 module.exports.signup_post = async (req, res) => {
-  const {email, password} = req.body
+    const {email, password} = req.body
 
-  try {
-    const user = await User.create({email, password})
-    res.status(201).json(user)
-  } catch (error) {
-    const errors = handleErrors(error)
-    res.status(400).json({errors})
-  }
+    try {
+        const user = await User.create({email, password})
+        const token = createJwt(user._id)
+        res.cookie('authToken', token, {httpOnly: true, maxAge: maxAge * 1000})
+        res.status(201).json(
+            {
+                user: user._id,
+            }
+        )
+    } catch (error) {
+        const errors = handleErrors(error)
+        res.status(400).json({errors})
+    }
 }
 
 module.exports.login_post = async (req, res) => {
-  res.send(req.body)
+    const {email, password} = req.body
+
+    try {
+        const user = await User.login(email, password)
+        const token = createJwt(user._id)
+        res.cookie('authToken', token, {httpOnly: true, maxAge: maxAge * 1000})
+        res.status(200).json(
+            {
+                user: user._id
+            }
+        )
+    } catch (error) {
+        const errors = handleErrors(error)
+        res.status(400).json({errors})
+    }
 }
